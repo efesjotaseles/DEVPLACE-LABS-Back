4650dd9a972151ecb6dbe683165c653cfdedb5d8
JBeltran19 <55241337+JBeltran19@users.noreply.github.com>
2023-02-01 15:54:11 -0300

    manejo de imagen con multer

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const publicationRouter = require("./routes/publication.route");
+const multer = require("multer");
+const path = require("path");
+const { fileURLToPath} = require("url");
 const authRouter = require("./routes/auth");
+const userRouter = require("./routes/user.routes");
+const postRouter = require("./routes/post");
+const {register} = require("./controllers/auth");
+const {createPost} = require("./controllers/post");
+const {verifyToken} = require("./middleware/auth");
+const bodyParser = require("body-parser");
+
 
 //ENV variables
 require("dotenv").config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 const port = process.env.PORT || 3030;
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
 //CORS
 const cors = require("cors");
@@ -25,10 +47,17 @@ mongoose
     console.log(error);
   });
 
+  /* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts" ,verifyToken,upload.single("picture"), createPost);
+
 /**ENDPOINTS */
 
-app.use("/publications", publicationRouter);
 app.use("/auth",authRouter);
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
