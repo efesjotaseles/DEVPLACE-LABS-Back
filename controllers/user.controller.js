0e1b4934308eaaab4c121700c0108055d56cdada
JBeltran19 <55241337+JBeltran19@users.noreply.github.com>
2023-02-01 15:45:04 -0300

    update controller user/friend

---
+++ controllers/user.controller.js
@@ -0,0 +1,70 @@
+const User = require('../models/User');
+
+/* READ */
+const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, fname, lname, occupation, location, picturePath }) => {
+        return { _id, fname, lname, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+/* UPDATE */
+const addRemoveFriend = async (req, res) => {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== id);
+    } else {
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+    await user.save();
+    await friend.save();
+    console.log(user.friends);
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, fname, lname, occupation, location, picturePath }) => {
+        return { _id, fname, lname, occupation, location, picturePath };
+      }
+    );
+
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+  module.exports = {
+    getUser,
+    getUserFriends,
+    addRemoveFriend,
+
+  };
